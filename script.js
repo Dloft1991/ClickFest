50eb831d376e86413c1a0e4fe39c05682bb90c2e
Dloft <derek.lofton1@gmail.com>
2019-12-06 17:22:21 -0500

    js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-tile = document.getElementById("tile");
+Tile = document.getElementById("tile1").style.color = "blue";
+Tile2 = document.getElementById("tile2").style.color = "blue";
 
 function change() {
-    alert("working");
-    
+    // alert("working");
+    Tile1 = document.getElementById("tile1").style.color = "green";
+}
+function change2() {
+    Tile2 = document.getElementById("tile2").style.color = "green";
 }
